f31c48e6c184a66be3b242189e68e1481ebf8636
allagaymen <a.allag@esi-sba.dz>
2024-10-30 18:03:11 +0100

    Create recipe with tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,6 +4,7 @@ Views for the recipe APIs
 """
 from django.shortcuts import render
 
+
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -34,7 +35,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin, 
+                 mixins.DestroyModelMixin, viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -44,3 +46,5 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    
